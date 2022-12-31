ee7ae4ac72dea902aeea7c490f2f84b3170803c4
Luciano Carvalho <lucianocarv13@gmail.com>
2022-12-31 14:22:51 -0300

    Login com JSON Web Token

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { jwtService } from "../services/jwtService";
 import { userService } from "../services/userService";
 
 export const authController = {
@@ -10,6 +11,27 @@ export const authController = {
 			const user = await userService.create({ firstName, lastName, email, password, birth, phone, role: "user" });
 
 			return res.status(201).json(user);
+		} catch (error) {
+			if (error instanceof Error) {
+				return res.status(400).json({ message: error.message });
+			}
+		}
+	},
+
+	login: async (req: Request, res: Response) => {
+		const { email, password } = req.body;
+		try {
+			const user = await userService.findByEmail(email);
+
+			if (!user) return res.status(401).json({ message: "E-mail não registrado!" });
+
+			user.checkPassword(password, (error, isSame) => {
+				if (error) return res.status(400).json({ message: error.message });
+				if (!isSame) return res.status(401).json({ message: "Senha incorreta" });
+				const payload = { id: user.id, firstName: user.firstName, email: user.email };
+				const token = jwtService.signToken(payload, "1d");
+				return res.json({ authenticated: true, ...payload, token });
+			});
 		} catch (error) {
 			if (error instanceof Error) {
 				return res.status(400).json({ message: error.message });
--- src/routes.ts
+++ src/routes.ts
@@ -6,6 +6,7 @@ import { episodesController } from "./controllers/episodesController";
 const router = Router();
 
 router.post("/auth/register", authController.register);
+router.post("/auth/login", authController.login);
 
 router.get("/categories", categoriesController.index);
 router.get("/categories/:id", categoriesController.show);
