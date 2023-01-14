4d8303a3a0a8ce7f037c84dd88ffbb271d4a4f68
Luciano Carvalho <lucianocarv13@gmail.com>
2023-01-14 20:30:05 -0300

    Excluindo um gostei

--- src/models/index.ts
+++ src/models/index.ts
@@ -3,9 +3,11 @@ import { Course } from "./Course";
 import { Episode } from "./Episode";
 import { User } from "./User";
 import { Favorite } from "./Favorite";
+import { Like } from "./Like";
 
 Category.hasMany(Course, { as: "courses" });
 Course.belongsTo(Category);
+Course.belongsToMany(User, { through: Like });
 
 Course.hasMany(Episode, { as: "episodes" });
 Course.belongsToMany(User, { through: Favorite });
@@ -16,6 +18,7 @@ Favorite.belongsTo(Course);
 Favorite.belongsTo(User);
 
 User.belongsToMany(Course, { through: Favorite });
+User.belongsToMany(Course, { through: Like });
 User.hasMany(Favorite, { as: "FavoritesCourse", foreignKey: "user_id" });
 
-export { Category, Course, Episode, User };
+export { Category, Course, Episode, User, Favorite, Like };
