2c8c19c45def73a1c44d67fef9c6062f9b243432
Luciano Carvalho <lucianocarv13@gmail.com>
2023-01-24 14:51:33 -0300

    Atualizando a senha

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -28,6 +28,24 @@ export const usersController = {
 		}
 	},
 
+	updatePassword: async (req: AuthenticatedRequest, res: Response) => {
+		const user = req.user!;
+		const { currentPassword, newPassword } = req.body;
+
+		try {
+			user.checkPassword(currentPassword, async (error, isSame) => {
+				if (error) return res.status(400).json({ message: error.message });
+				if (!isSame) return res.status(400).json({ message: "Senha incorreta!" });
+				await userService.updatePassword(user.id, newPassword);
+				return res.status(204).send();
+			});
+		} catch (error) {
+			if (error instanceof Error) {
+				return res.status(400).json({ message: error.message });
+			}
+		}
+	},
+
 	watching: async (req: AuthenticatedRequest, res: Response) => {
 		const { id } = req.user!;
 
